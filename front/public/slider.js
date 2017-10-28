82beb313a2a29d8471c238770db3d996f7243def
thifranc <thifranc@student.42.fr>
2017-10-28 16:42:55 +0200

    advanced js

--- front/public/slider.js
+++ front/public/slider.js
@@ -1,4 +1,3 @@
-var current = [];
 
 function initiateSquare(array){
 	current = array;
@@ -19,11 +18,10 @@ function initiateSquare(array){
 }
 
 function swapBoxes(change){
-
+	console.log(`change is at ${change}`);
 	var allBoxes = document.getElementsByClassName("tile");
 	for (var i = 0; i < allBoxes.length; i++)
 	{
-		console.log(allBoxes[i].innerHTML);
 		if (allBoxes[i].innerHTML == 0)
 		{
 			allBoxes[i].className = "tile";
@@ -38,19 +36,82 @@ function swapBoxes(change){
 }
 
 function newSquare(array){
-	
+
+	console.log(`this is arr received ${array}`);
 	for (var i = 0; i < 3; i++)
 	{
 		for (var j = 0; j < 3; j++)
 		{
 			if (array[i][j] !== current[i][j])
 			{
-				var change = array[i][j] == 0 ?
+				var change = array[i][j] === 0 ?
 					current[i][j] :
 					array[i][j];
 			}
 		}
 	}
 	swapBoxes(change);
+	current = array;
+}
 
+function showSteps(steps, i){
+	console.log(`steps recevied - ${steps}`);
+	if (i === steps.length)
+		return ;
+	
+	setTimeout(function () {
+		newSquare(steps[i]);
+		showSteps(steps, i + 1);
+	}, 1000);
+}
+
+
+function all(){
+	document.getElementById("generate").onclick = () => {
+
+		var request = new XMLHttpRequest();
+		request.open('GET', '/newSqr', true);
+		
+		request.onload = function() {
+		  if (request.status >= 200 && request.status < 400) {
+			var newSqr = JSON.parse(request.responseText);
+			initiateSquare(newSqr.sqr);
+		  } else {
+			console.log('PB lol', request);
+		  }
+		};
+		request.onerror = function() {
+			console.log('real error');
+		};
+		
+		request.send();
+	};
+
+	document.getElementById("solve").onclick = () => {
+
+		var request = new XMLHttpRequest();
+		request.open('GET', '/solve', true);
+		
+		request.onload = function() {
+		  if (request.status >= 200 && request.status < 400) {
+			var solution = JSON.parse(request.responseText);
+			showSteps(solution.steps, 1);
+		  } else {
+			console.log('PB lol', request);
+		  }
+		};
+		request.onerror = function() {
+			console.log('real error');
+		};
+		
+		request.send();
+	};
+}
+
+function ready(all) {
+	if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+		all();
+	} else {
+		document.addEventListener('DOMContentLoaded', all);
+	}
 }
--- front/server.js
+++ front/server.js
@@ -8,4 +8,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/base.html'));
 })
 
+app.get('/newSqr', function (req, res) {
+  res.sendFile(path.join(__dirname + '/newSqr.json'));
+})
+
+app.get('/solve', function (req, res) {
+  res.sendFile(path.join(__dirname + '/solved.json'));
+})
+
 app.listen(3000);
