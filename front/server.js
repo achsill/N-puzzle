bd602c7213addcaa7b2c8a91167f464337b53da3
thifranc <thifranc@student.42.fr>
2017-10-28 16:45:08 +0200

    hamza need explain use of py files

--- front/server.js
+++ front/server.js
@@ -9,10 +9,14 @@ app.get('/', function (req, res) {
 })
 
 app.get('/newSqr', function (req, res) {
+	//doit launch generate .py
   res.sendFile(path.join(__dirname + '/newSqr.json'));
 })
 
 app.get('/solve', function (req, res) {
+	//sera du POST car conf de resolution a passer
+	//doit launch solve .py avec newSqr.json en param
+	// -> genere un solved .json -> renvoie
   res.sendFile(path.join(__dirname + '/solved.json'));
 })
 
